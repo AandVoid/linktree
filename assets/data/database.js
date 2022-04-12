625671d0ec204ca35d44dc6b74d679dd06c6650e
AandVoid <aand.void@gmail.com>
2022-04-11 23:12:34 -0300

    update layout, add international store, language condition

---
+++ assets/data/database.js
@@ -0,0 +1,32 @@
+const language = {
+    "english": {
+        "language": "english",
+        "title": "AndVøid",
+        "email": "aandvoid@gmail.com",
+        "role": "Digital artist from Brazil",
+        "roleDescription": "I work with a mix of ukiyo-e, comic book and street art styles",
+        "portfolioAction": "View Portfolio",
+        "commissionAction": "Contact for Commissions",
+        "springStoreTitle": "Spring Online Store",
+        "springStoreSubtitle": "Prints, Mugs, T-shirts and much more...",
+        "springStoreDescription": "International Store",
+        "localStoreTitle": "Local Online Store",
+        "localStoreSubtitle": "Signed prints",
+        "localStoreDescription": "Delivered on Brazil only"
+    },
+    "portuguese": {
+        "language": "portuguese",
+        "title": "AndVøid",
+        "email": "aandvoid@gmail.com1",
+        "role": "Artista Digital Brasileiro",
+        "roleDescription": "Eu trabalho com uma mistura de estilos: ukiyo-e, quadrinhos e arte de rua",
+        "portfolioAction": "Ver portfólio",
+        "commissionAction": "Contato para encomendas",
+        "springStoreTitle": "Spring online",
+        "springStoreSubtitle": "Impressões, canecas, camisetas e mais...",
+        "springStoreDescription": "Loja Internacional",
+        "localStoreTitle": "Loja online Local",
+        "localStoreSubtitle": "Impressões A3 assinadas",
+        "localStoreDescription": "Entregas apenas no Brasil"
+    },
+};
---
+++ assets/data/index.js
@@ -0,0 +1,46 @@
+var userLanguage = window.navigator.userLanguage || window.navigator.language;
+
+console.log(userLanguage);
+
+// Start
+window.onload = () => populatePage();
+
+
+//Copy Email
+function myFunction() {
+    var copyText = language.english.email
+    navigator.clipboard.writeText(copyText); 
+    var tooltip = document.getElementById("commission-action");
+    tooltip.innerHTML = "🐸🐸 Copied e-mail address 🐸🐸";
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("commission-action");
+    setTimeout(() => { tooltip.innerHTML = language.english.commissionAction; }, 2000);
+}
+
+function accessStore(url) {
+    window.open(url, '_blank').focus();
+}
+
+
+function populatePage(userLanguage){
+    userLanguage != "pt-BR" ?
+    printData(language.english) : 
+    printData(language.portuguese)  
+}
+
+function printData(language){
+    console.log("printData")
+    document.getElementById("email").innerHTML=language.email;
+    document.getElementById("role").innerHTML=language.role;
+    document.getElementById("role-description").innerHTML=language.roleDescription;
+    document.getElementById("portfolio-action").innerHTML=language.portfolioAction;
+    document.getElementById("commission-action").innerHTML=language.commissionAction;
+    document.getElementById("spring-store-title").innerHTML=language.springStoreTitle;
+    document.getElementById("spring-store-subtitle").innerHTML=language.springStoreSubtitle;
+    document.getElementById("spring-store-description").innerHTML=language.springStoreDescription;
+    document.getElementById("local-store-title").innerHTML=language.localStoreTitle;
+    document.getElementById("local-store-subtitle").innerHTML=language.localStoreSubtitle;
+    document.getElementById("local-store-description").innerHTML=language.localStoreDescription;
+}
